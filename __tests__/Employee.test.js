edad7ca37453708708937cf5de243fb5beb10eee
bpiper91 <bpiper91@gmail.com>
2022-08-01 11:02:46 -0400

    added Employee object constructor and methods

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,2 +1,19 @@
 // link to Employee.js
 const Employee = require('../lib/Employee');
+
+test('creates an employee object', () => {
+    const employee = new Employee('Jane', 2, 'jane@doe.com');
+
+    expect(employee.name).toBe('Jane');
+    expect(employee.id).toBe(2);
+    expect(employee.email).toBe('jane@doe.com');
+});
+
+test('returns name, id, email, and role', () => {
+    const employee = new Employee('John', 5, 'john@doe.com');
+
+    expect(employee.getName()).toBe('John');
+    expect(employee.getId()).toBe('5'); // no longer number in template literal
+    expect(employee.getEmail()).toBe('john@doe.com');
+    expect(employee.getRole()).toBe('Employee');
+});
--- index.js
+++ index.js
@@ -1,3 +1,2 @@
 // load inquirer module for user input
 const inquirer = require('inquirer');
-
